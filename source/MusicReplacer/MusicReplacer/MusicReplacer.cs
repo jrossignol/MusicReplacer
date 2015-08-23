b4d75931c9322d41eb86482f3c6600ca5a8eccde
Jeremie Rossignol <jrossignol@gmail.com>
2015-08-23 08:49:33 -0600

    Initial revision.

---
+++ source/MusicReplacer/MusicReplacer/MusicReplacer.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using KSP;
+
+namespace MusicReplacer
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class MusicReplacer : MusicLogic
+    {
+        public enum Theme
+        {
+            menuTheme,
+            menuAmbience,
+            credits,
+            trackingAmbience,
+            spaceCenterAmbience,
+            VABAmbience,
+            SPHAmbience,
+            astroComplexAmbience,
+            researchComplexAmbience,
+            missionControlAmbience,
+            adminFacilityAmbience,
+            constructionPlaylist,
+            spacePlaylist,
+        }
+
+        public class Replacement
+        {
+            public Theme theme;
+            public AudioClip clip;
+            public CelestialBody body;
+            public float minAltitude;
+            public float maxAltitude = float.MaxValue;
+
+            public override string ToString()
+            {
+                return this == null || clip == null ? "null" : clip.name;
+            }
+        }
+
+        public static MusicReplacer Instance;
+        public List<Replacement> replacements = new List<Replacement>();
+        public List<AudioClip> originalMusic = null;
+
+        protected IEnumerable<Replacement> currentReplacements = Enumerable.Empty<Replacement>();
+
+        public bool loaded = false;
+        float lastUpdate = 0.0f;
+        bool isInDefaultMode = true;
+
+        double spaceAltitude;
+        AudioSource fadeOutAudio = null;
+
+        void Awake()
+        {
+            if (fetch != null)
+            {
+                if (fetch.GetType() != typeof(MusicReplacer))
+                {
+                    Debug.Log("[MusicReplacer] Replacing existing MusicLogic");
+
+                    // Don't know where to find the prefab, so copy from the existing MusicLogic
+                    menuTheme = fetch.menuTheme;
+                    menuAmbience = fetch.menuAmbience;
+                    credits = fetch.credits;
+                    trackingAmbience = fetch.trackingAmbience;
+                    spaceCenterAmbience = fetch.spaceCenterAmbience;
+                    VABAmbience = fetch.VABAmbience;
+                    SPHAmbience = fetch.SPHAmbience;
+                    astroComplexAmbience = fetch.astroComplexAmbience;
+                    researchComplexAmbience = fetch.researchComplexAmbience;
+                    missionControlAmbience = fetch.missionControlAmbience;
+                    adminFacilityAmbience = fetch.adminFacilityAmbience;
+                    constructionPlaylist = fetch.constructionPlaylist;
+                    spacePlaylist = fetch.spacePlaylist;
+                    audio1 = fetch.audio1;
+                    audio2 = fetch.audio2;
+
+                    DestroyImmediate(fetch);
+                    fetch = null;
+                }
+                else
+                {
+                    Debug.LogWarning("[MusicReplacer] Not creating duplicate MusicReplacer.");
+                    DestroyImmediate(this);
+                    return;
+                }
+            }
+
+            Debug.Log("[MusicReplacer] Setting up MusicLogic copy...");
+            MethodInfo awakeMethod = typeof(MusicLogic).GetMethod("Awake", BindingFlags.Instance | BindingFlags.NonPublic);
+            awakeMethod.Invoke(this, new object[] { });
+            Instance = this;
+
+            Debug.Log("[MusicReplacer] Hooking into loading subsystem...");
+            LoadingScreen screen = FindObjectOfType<LoadingScreen>();
+            if (screen == null)
+            {
+                Debug.LogError("[MusicReplacer] Couldn't find LoadingScreen!");
+                return;
+            }
+
+            List<LoadingSystem> loadList = LoadingScreen.Instance.loaders;
+            if (loadList == null)
+            {
+                Debug.LogError("[MusicReplacer] Couldn't find loaders!");
+                return;
+            }
+        }
+
+        void Update()
+        {
+            // Would love to do this as a proper loader, but FlightGlobals.Bodies is not initialized then
+            if (HighLogic.LoadedScene == GameScenes.MAINMENU && !loaded)
+            {
+                MusicReplacerLoader.PerformLoad();
+                loaded = true;
+
+                spaceAltitude = FlightGlobals.GetHomeBody().atmosphereDepth;
+
+                // The stock prefab has some odd settings:
+                //   - Turn doppler off, as this makes things sound weird when the camera zooms or moves
+                //   - Change the min/max distance so that we can always hear the music
+                audio1.dopplerLevel = 0;
+                audio1.minDistance = float.MaxValue;
+                audio1.maxDistance = float.MaxValue;
+            }
+            else if (HighLogic.LoadedScene == GameScenes.FLIGHT && audio1 != null && FlightGlobals.ActiveVessel != null)
+            {
+                if (lastUpdate + 0.5f < Time.fixedTime)
+                {
+                    Debug.Log("In update loop (" + audio1.isPlaying + ")");
+                    lastUpdate = Time.fixedTime;
+
+                    IEnumerable<Replacement> selections = replacements.Where(r =>
+                        r.theme == Theme.spacePlaylist &&
+                        (r.body == null || r.body == FlightGlobals.currentMainBody) &&
+                        FlightGlobals.ActiveVessel.altitude >= r.minAltitude &&
+                        FlightGlobals.ActiveVessel.altitude < r.maxAltitude
+                    );
+
+                    bool replacementsChanged = selections.Where(r => !currentReplacements.Contains(r)).Any() ||
+                        currentReplacements.Where(r => !selections.Contains(r)).Any();
+
+                    if (replacementsChanged)
+                    {
+                        Debug.Log("    replacement list has changed");
+                        currentReplacements = selections.ToList();
+                        spacePlaylist = currentReplacements.Any() ? currentReplacements.Select(r => r.clip).ToList() : originalMusic;
+                    }
+                    Debug.Log("    first replacement is: " + currentReplacements.FirstOrDefault());
+
+                    if (isInDefaultMode && currentReplacements.Any())
+                    {
+                        Debug.Log("    switching to non-default mode...");
+                        isInDefaultMode = false;
+                        RestartMusic();
+                    }
+                    else if (!isInDefaultMode)
+                    {
+                        if (!currentReplacements.Any())
+                        {
+                            Debug.Log("    switching to back to default mode...");
+                            isInDefaultMode = true;
+                            RestartMusic();
+                        }
+                        else if (!spacePlaylist.Contains(audio1.clip) && replacementsChanged)
+                        {
+                            Debug.Log("    no mode change...");
+                            RestartMusic();
+                        }
+                    }
+
+                    if (audio1.clip != null)
+                    {
+                        Debug.Log("    audio1 = " + audio1.clip.name);
+                    }
+                }
+            }
+            else if (HighLogic.LoadedScene != GameScenes.FLIGHT && !isInDefaultMode)
+            {
+                flightMusicSpaceAltitude = spaceAltitude;
+                isInDefaultMode = true;
+            }
+        }
+
+        protected void RestartMusic(bool crossfade = false)
+        {
+            Debug.Log("    restarting music...");
+            flightMusicSpaceAltitude = isInDefaultMode ? spaceAltitude : 0.0;
+            StopAllCoroutines();
+            if (audio1.isPlaying)
+            {
+                StartCoroutine(DoCrossFade(1.0f));
+            }
+            audio1.Stop();
+            StartCoroutine("PlayFlight");
+        }
+
+        protected IEnumerator<YieldInstruction> DoCrossFade(float duration)
+        {
+            if (fadeOutAudio != null)
+            {
+                fadeOutAudio.Stop();
+                Destroy(fadeOutAudio);
+                fadeOutAudio = null;
+            }
+
+            fadeOutAudio = (AudioSource)UnityEngine.Object.Instantiate(audio1);
+
+            float startTime = Time.time;
+            float endTime = startTime + duration;
+            while (Time.time < endTime)
+            {
+                float ratio = (Time.time - startTime) / duration;
+                audio1.volume = GameSettings.MUSIC_VOLUME * (1.0f - ratio);
+                fadeOutAudio.volume = GameSettings.MUSIC_VOLUME * (ratio);
+                yield return null;
+            }
+            audio1.volume = GameSettings.MUSIC_VOLUME;
+            fadeOutAudio.Stop();
+            Destroy(fadeOutAudio);
+            fadeOutAudio = null;
+        }
+    }
+}
---
+++ source/MusicReplacer/MusicReplacer/MusicReplacerLoader.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using KSP;
+
+namespace MusicReplacer
+{
+    public class MusicReplacerLoader
+    {
+        public static void PerformLoad()
+        {
+            ConfigNode[] musicNodes = GameDatabase.Instance.GetConfigNodes("MUSIC_REPLACER");
+            IEnumerable<ConfigNode> nodes = musicNodes.SelectMany(cn => cn.GetNodes("MUSIC"));
+
+            bool removeConstruction = false;
+            bool removeSpace = false;
+            int constructionCount = MusicReplacer.Instance.constructionPlaylist.Count;
+            int spaceCount = MusicReplacer.Instance.spacePlaylist.Count;
+
+            foreach (ConfigNode node in nodes)
+            {
+                MusicReplacer.Replacement replacement = new MusicReplacer.Replacement();
+                try
+                {
+                    bool valid = true;
+
+                    if (!node.HasValue("name"))
+                    {
+                        Debug.LogError("[MusicReplacer] Couldn't load MUSIC node - no name attribute found");
+                        valid = false;
+                    }
+                    else
+                    {
+                        replacement.theme = (MusicReplacer.Theme)Enum.Parse(typeof(MusicReplacer.Theme), node.GetValue("name"));
+                    }
+
+                    if (!node.HasValue("musicURL"))
+                    {
+                        Debug.LogError("[MusicReplacer] Couldn't load MUSIC node - no musicURL attribute found");
+                        valid = false;
+                    }
+                    else
+                    {
+                        string url = node.GetValue("musicURL");
+                        replacement.clip = GameDatabase.Instance.databaseAudio.Where(a => a.name == url).FirstOrDefault();
+                        if (replacement.clip == null)
+                        {
+                            Debug.LogError("[MusicReplacer] Couldn't find audio file at URL: " + url);
+                            valid = false;
+                        }
+                    }
+
+                    bool removeExisting = false;
+                    if (node.HasValue("removeExisting"))
+                    {
+                        removeExisting = bool.Parse(node.GetValue("removeExisting"));
+                    }
+
+                    if (node.HasValue("celestialBody"))
+                    {
+                        if (replacement.theme != MusicReplacer.Theme.spacePlaylist)
+                        {
+                            Debug.LogError("[MusicReplacer] The celestialBody attribute is only valid for spacePlaylist.");
+                            valid = false;
+                        }
+                        else
+                        {
+                            string bodyName = node.GetValue("celestialBody");
+                            replacement.body = FlightGlobals.Bodies.Where(cb => cb.name == bodyName).FirstOrDefault();
+                            if (replacement.body == null)
+                            {
+                                Debug.LogError("[MusicReplacer] Couldn't find celestial body named '" + bodyName + "'.");
+                                valid = false;
+                            }
+                        }
+                    }
+
+                    if (node.HasValue("minAltitude"))
+                    {
+                        if (replacement.theme != MusicReplacer.Theme.spacePlaylist)
+                        {
+                            Debug.LogError("[MusicReplacer] The minAltitude attribute is only valid for spacePlaylist.");
+                            valid = false;
+                        }
+                        else
+                        {
+                            replacement.minAltitude = (float)Double.Parse(node.GetValue("minAltitude"));
+                        }
+                    }
+
+                    if (node.HasValue("maxAltitude"))
+                    {
+                        if (replacement.theme != MusicReplacer.Theme.spacePlaylist)
+                        {
+                            Debug.LogError("[MusicReplacer] The maxAltitude attribute is only valid for spacePlaylist.");
+                            valid = false;
+                        }
+                        else
+                        {
+                            replacement.maxAltitude = (float)Double.Parse(node.GetValue("maxAltitude"));
+                        }
+                    }
+
+                    if (valid)
+                    {
+                        Debug.Log("[MusicReplacer] Loaded a MUSIC node for " + replacement.theme);
+
+                        // Perform a simple replacement
+                        if (replacement.theme != MusicReplacer.Theme.spacePlaylist ||
+                            replacement.body == null && replacement.minAltitude == 0.0f && replacement.maxAltitude == float.MaxValue)
+                        {
+                            switch (replacement.theme)
+                            {
+                                case MusicReplacer.Theme.menuTheme:
+                                    MusicReplacer.Instance.menuTheme = replacement.clip;
+                                    break;
+                                case MusicReplacer.Theme.menuAmbience:
+                                    MusicReplacer.Instance.menuAmbience = replacement.clip;
+                                    break;
+                                case MusicReplacer.Theme.credits:
+                                    MusicReplacer.Instance.credits = replacement.clip;
+                                    break;
+                                case MusicReplacer.Theme.trackingAmbience:
+                                    MusicReplacer.Instance.trackingAmbience = replacement.clip;
+                                    break;
+                                case MusicReplacer.Theme.spaceCenterAmbience:
+                                    MusicReplacer.Instance.spaceCenterAmbience = replacement.clip;
+                                    break;
+                                case MusicReplacer.Theme.VABAmbience:
+                                    MusicReplacer.Instance.VABAmbience = replacement.clip;
+                                    break;
+                                case MusicReplacer.Theme.SPHAmbience:
+                                    MusicReplacer.Instance.SPHAmbience = replacement.clip;
+                                    break;
+                                case MusicReplacer.Theme.astroComplexAmbience:
+                                    MusicReplacer.Instance.astroComplexAmbience = replacement.clip;
+                                    break;
+                                case MusicReplacer.Theme.researchComplexAmbience:
+                                    MusicReplacer.Instance.researchComplexAmbience = replacement.clip;
+                                    break;
+                                case MusicReplacer.Theme.missionControlAmbience:
+                                    MusicReplacer.Instance.missionControlAmbience = replacement.clip;
+                                    break;
+                                case MusicReplacer.Theme.adminFacilityAmbience:
+                                    MusicReplacer.Instance.adminFacilityAmbience = replacement.clip;
+                                    break;
+                                case MusicReplacer.Theme.constructionPlaylist:
+                                    MusicReplacer.Instance.constructionPlaylist.Add(replacement.clip);
+                                    break;
+                                case MusicReplacer.Theme.spacePlaylist:
+                                    MusicReplacer.Instance.spacePlaylist.Add(replacement.clip);
+                                    break;
+                            }
+                        }
+                        // Add to our lists
+                        else
+                        {
+                            MusicReplacer.Instance.replacements.Add(replacement);
+                        }
+
+                        // Flag the original lists for removal
+                        if (replacement.theme == MusicReplacer.Theme.spacePlaylist)
+                        {
+                            removeSpace |= removeExisting;
+                        }
+                        else if (replacement.theme == MusicReplacer.Theme.constructionPlaylist)
+                        {
+                            removeConstruction |= removeExisting;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.Log("[MusicReplacer] Exception trying to load MUSIC node:");
+                    Debug.LogException(e);
+                }
+            }
+
+            // Remove existing values, if required
+            if (removeConstruction)
+            {
+                MusicReplacer.Instance.constructionPlaylist.RemoveRange(0, constructionCount);
+            }
+            if (removeSpace)
+            {
+                MusicReplacer.Instance.spacePlaylist.RemoveRange(0, spaceCount);
+            }
+
+            // Get the original in-flight list saved
+            MusicReplacer.Instance.originalMusic = MusicReplacer.Instance.spacePlaylist.ToList();
+
+            MusicReplacer.Instance.loaded = true;
+        }
+    }
+}
